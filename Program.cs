3eebd3a76bbf45d64e65f8b97a4fd41dc89863b0
PetterC <petterchan89@gmail.com>
2016-11-14 07:36:09 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            try
+            {
+                IPAddress ipAd = IPAddress.Parse("10.2.20.38");
+
+                TcpListener myList = new TcpListener(ipAd, 8000);
+
+                myList.Start();
+
+                Console.WriteLine("The server is running at port 8000...");
+                Console.WriteLine("The local End point is  :" + myList.LocalEndpoint);
+                Console.WriteLine("Waiting for a connection.....");
+
+                Socket s = myList.AcceptSocket();
+                Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+
+                byte[] b = new byte[100];
+                int k = s.Receive(b);
+                Console.WriteLine("Recieved...");
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(b[i]));
+
+                ASCIIEncoding asen = new ASCIIEncoding();
+                s.Send(asen.GetBytes("The string was recieved by the server."));
+                Console.WriteLine("\nSent Acknowledgement");
+                Console.ReadLine();
+
+                s.Close();
+                myList.Stop();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+
+
+            }
+
+           
+
+
+
+
+        }
+    }
+}
